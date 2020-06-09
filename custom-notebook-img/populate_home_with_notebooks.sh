6188191ede1af305c0fa526f625b55200be2eeac
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-06-09 22:08:53 +0200

    Ensure bash notebook has its kernel activated

--- custom-notebook-img/populate_home_with_notebooks.sh
+++ custom-notebook-img/populate_home_with_notebooks.sh
@@ -5,6 +5,9 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 
+# Ensure that bash kernel is indeed installed
+python -m bash_kernel.install
+
 if [ ! -d /home/jovyan/cue-lang-demo ]; then
   git -c http.sslVerify=false clone https://github.com/AleksanderGondek/cue-lang-demo.git /home/jovyan/cue-lang-demo
 fi

5df5c0c88c276061f631b6c873bc53431a647ce4
Aleksander Gondek <aleksander.gondek@dynatrace.com>
2020-06-10 19:49:09 +0200

    Ensure jupyter helm chart is present while generating deployment

--- deployment/generate-deployment.sh
+++ deployment/generate-deployment.sh
@@ -4,6 +4,9 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
+
 helm template jupyterhub/jupyterhub \
   --name-template '0.0.4' \
   --namespace 'cue-lang-demo' \
